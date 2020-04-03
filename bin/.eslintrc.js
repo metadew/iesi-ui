8eff7144016ba02bdb2c0539338cd9da545b2f9d
Ben Verbist <ben.verbist@kunstmaan.be>
2020-04-03 14:58:27 +0200

    [setup] fix unit test + prepare bin scripts

---
+++ bin/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        'import/no-extraneous-dependencies': ['off'],
+        '@typescript-eslint/no-var-requires': ['off'],
+    },
+};
---
+++ bin/paths.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require('fs');
+
+// Make sure any symlinks in the project folder are resolved:
+// https://github.com/facebookincubator/create-react-app/issues/637
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+module.exports = {
+    appBuild: resolveApp('build'),
+    appPublic: resolveApp('public'),
+};
