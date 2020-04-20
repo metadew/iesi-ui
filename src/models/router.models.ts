2bdc4badeeee1f1c931079d117415f719c6e56cf
Ben Verbist <ben.verbist@kunstmaan.be>
2020-04-20 11:57:01 +0200

    [routing] added small comment

--- src/models/router.models.ts
+++ src/models/router.models.ts
@@ -11,6 +11,6 @@ export interface IRoute<RouteKey = string> extends RouteProps {
     path: string;
     allowAnonymousAccess?: boolean; // default true // TODO default false once authentication mechanism
     requiredAccessLevels?: Partial<IAccessLevel>;
-    template?: React.ElementType;
+    template?: React.ElementType; // only used for parent routes (takes precedence there over the component prop)
     childRoutes?: IRoute<RouteKey>[];
 }
