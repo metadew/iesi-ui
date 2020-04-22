197269306df3605cdf311893882b0385f122ff81
vdwjeff <vdwjeff@gmail.com>
2020-04-22 13:25:11 +0200

    Added navigation menu to header

--- src/views/appShell/AppHeader/index.tsx
+++ src/views/appShell/AppHeader/index.tsx
@@ -11,10 +11,13 @@ import {
     Theme,
     Box,
 } from '@material-ui/core';
-import { Menu as MenuIcon, Brightness4 as BrightnessIcon } from '@material-ui/icons';
+import { Brightness4 as BrightnessIcon } from '@material-ui/icons';
 import { grey } from '@material-ui/core/colors';
+import RouteLink from 'views/common/navigation/RouteLink';
 import { TThemeName } from 'config/theme.config';
+import { ROUTE_KEYS } from 'views/routes';
 import packageJson from '../../../../package.json';
+import NavigationMenu from './NavigationMenu';
 
 interface IPublicProps {
     toggleTheme: () => void;
@@ -33,17 +36,19 @@ const styles = ({ spacing, palette }: Theme) => createStyles({
         fontWeight: 700,
         margin: spacing(1),
     },
-    brand: {
+    brandContainer: {
         flexGrow: 1,
+    },
+    brand: {
+        display: 'inline-block',
     },
     action: {
         paddingLeft: spacing(1),
         paddingRight: spacing(1),
         borderLeft: '1px solid',
         borderLeftColor: grey[200],
-    },
-    icon: {
         color: palette.primary.dark,
+
     },
 });
 
@@ -54,21 +59,23 @@ function AppHeader({
     return (
         <AppBar position="static" className={classes.appBar}>
             <Toolbar>
-                <Box display="flex" flexDirection="row" alignItems="baseline" className={classes.brand}>
-                    <Typography variant="h4" className={classes.title}>
-                        <Translate msg="app_shell.header.title" raw />
-                    </Typography>
-                    <span>{packageJson.version}</span>
-                </Box>
+                <div className={classes.brandContainer}>
+                    <RouteLink to={ROUTE_KEYS.R_HOME} className={classes.brand}>
+                        <Box display="flex" flexDirection="row" alignItems="baseline">
+                            <Typography variant="h4" className={classes.title}>
+                                <Translate msg="app_shell.header.title" raw />
+                            </Typography>
+                            <span>{packageJson.version}</span>
+                        </Box>
+                    </RouteLink>
+                </div>
                 <div className={classes.action}>
-                    <IconButton className={classes.icon} aria-label="theme-toggle" onClick={toggleTheme}>
+                    <IconButton aria-label="theme-toggle" onClick={toggleTheme}>
                         <BrightnessIcon />
                     </IconButton>
                 </div>
                 <div className={classes.action}>
-                    <IconButton className={classes.icon} aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+                    <NavigationMenu />
                 </div>
             </Toolbar>
         </AppBar>
