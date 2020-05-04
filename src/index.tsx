f4b9ee7df066aab2fd708512968bd3b9df8ca6e1
vdwjeff <vdwjeff@gmail.com>
2020-05-04 15:29:07 +0200

    Remove strict mode wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { ROOT_ELEMENT_ID } from './config/dom.config';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <App />,
     document.getElementById(ROOT_ELEMENT_ID),
 );
 
