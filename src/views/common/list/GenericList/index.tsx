e210554b834b5c5ec14203402feb2989f86b1f39
vdwjeff <vdwjeff@gmail.com>
2020-05-05 15:43:28 +0200

    Moved TableRow into separate component

--- src/views/common/list/GenericList/index.tsx
+++ src/views/common/list/GenericList/index.tsx
@@ -8,7 +8,6 @@ import Paper from '@material-ui/core/Paper';
 import {
     IListItem,
     ListColumns,
-    IColumn,
     IListAction,
     ISortedColumn,
     ListFilters,
@@ -16,27 +15,19 @@ import {
 import {
     TableCell,
     Typography,
-    IconButton,
     Theme,
-    Box,
-    Tooltip,
-    Icon,
 } from '@material-ui/core';
 import {
     Pagination,
     PaginationItem,
 } from '@material-ui/lab';
-import {
-    Info,
-} from '@material-ui/icons';
 import sortListItems from 'utils/list/sortListItems';
 import { filterListItems } from 'utils/list/filters';
-import { getListItemValueFromColumn } from 'utils/list/list';
 import { TObjectWithProps } from 'models/core.models';
 import AppTemplateContainer from 'views/appShell/AppTemplateContainer';
 import Translate from '@snipsonian/react/es/components/i18n/Translate';
+import GenericTableRow from './GenericTableRow';
 
-const SHORTEN_VALUE_FROM_CHARACTERS = 40;
 const ROWS_PER_PAGE = 5;
 
 interface IPublicProps<ColumnNames> {
@@ -71,21 +62,6 @@ const useStyles = makeStyles(({ palette, spacing, shape }: Theme) => ({
                 borderBottomRightRadius: shape.borderRadius,
             },
         },
-    },
-    tableRow: {
-        background: palette.background.paper,
-        boxShadow: '0 2px 22px rgba(0, 0, 0, .10)',
-        borderRadius: shape.borderRadius,
-    },
-    label: {
-        fontSize: '.8rem',
-        color: palette.grey[500],
-    },
-    action: {
-        width: 50,
-    },
-    actionIcon: {
-        color: palette.primary.dark,
     },
     paginationSelected: {
         backgroundColor: `${palette.primary.main} !important`,
@@ -136,64 +112,12 @@ export default function GenericList<ColumnNames>({
                             </TableRow>
                         )}
                         {itemsToDisplay.map((item: IListItem<ColumnNames>) => (
-                            <TableRow className={classes.tableRow} key={item.id}>
-                                {Object.keys(columns).map((untypedColumnName) => {
-                                    const columnName = (untypedColumnName as unknown) as keyof ColumnNames;
-                                    const column = columns[columnName] as IColumn<ColumnNames>;
-
-                                    const value = getListItemValueFromColumn(item, columnName).toString();
-                                    const shortenedValue = value.length > SHORTEN_VALUE_FROM_CHARACTERS
-                                        ? `${value.substr(0, SHORTEN_VALUE_FROM_CHARACTERS)}...`
-                                        : value;
-
-                                    const className = typeof column.className === 'function'
-                                        ? column.className(value)
-                                        : column.className;
-
-                                    const tooltip = typeof column.tooltip === 'function'
-                                        ? column.tooltip(value)
-                                        : column.tooltip;
-
-                                    return (
-                                        <TableCell style={{ width: column.fixedWidth }} key={columnName as string}>
-                                            <Typography
-                                                display="block"
-                                                className={classes.label}
-                                            >
-                                                {column.label}
-                                            </Typography>
-                                            <Box display="flex" alignItems="center">
-                                                <Typography className={className}>
-                                                    {shortenedValue}
-                                                </Typography>
-                                                {tooltip && (
-                                                    <Box marginLeft={1}>
-                                                        <Tooltip title={tooltip}>
-                                                            <Icon aria-label="info">
-                                                                <Info />
-                                                            </Icon>
-                                                        </Tooltip>
-                                                    </Box>
-                                                )}
-                                            </Box>
-                                        </TableCell>
-                                    );
-                                })}
-                                {listActions.map((action, index) => (
-                                    <TableCell // eslint-disable-next-line react/no-array-index-key
-                                        key={index}
-                                        align="right"
-                                        className={classes.action}
-                                    >
-                                        <IconButton
-                                            onClick={() => action.onClick(item.id)}
-                                            className={classes.actionIcon}
-                                        >
-                                            {action.icon}
-                                        </IconButton>
-                                    </TableCell>
-                                ))}
-                            </TableRow>
+                            <GenericTableRow
+                                key={item.id}
+                                item={item}
+                                listActions={listActions}
+                                columns={columns}
+                            />
                         ))}
                     </TableBody>
                 </Table>
