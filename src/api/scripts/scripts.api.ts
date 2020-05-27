76e6407cd13814f7e3813b7f65ef00364bae7081
Ben Verbist <ben.verbist@kunstmaan.be>
2020-05-27 14:40:46 +0200

    [api's] updated the script api's to the newest version

--- src/api/scripts/scripts.api.ts
+++ src/api/scripts/scripts.api.ts
@@ -1,50 +1,77 @@
+import { IUrlParams } from '@snipsonian/core/src/url/types';
 import {
+    IFetchScriptsOptions,
     IScriptBase,
-    IScriptWithVersions,
     IScript,
     IScriptByNamePayload,
-    IScriptByNameAndVersionPayload,
+    IScriptByNameAndVersionPayload, IExpandScriptsResponseWith,
 } from 'models/state/scripts.models';
 import { IListResponse } from 'models/state/iesiGeneric.models';
 import { get, post, put, remove } from '../requestWrapper';
 import API_URLS from '../apiUrls';
 
-export function fetchScripts() {
-    return get<IScriptBase[], IListResponse<IScriptBase>>({
+/**
+ * TODO This call currently returns a separate item for each version of a script.
+ * But in the edit-list we only show the latest version of a script.
+ * --> ideally Robbe adds a query param to conditionally get only the latest script versions
+ *     otherwise we have to filter them ourselves
+ */
+export function fetchScripts({ expandResponseWith }: IFetchScriptsOptions = {}) {
+    return get<IScript[], IListResponse<IScript>>({
         url: API_URLS.SCRIPTS,
+        queryParams: toExpandQueryParam(expandResponseWith),
         // eslint-disable-next-line no-underscore-dangle
         mapResponse: ({ data }) => data._embedded,
     });
 }
 
-export function fetchScriptVersions({ name }: IScriptByNamePayload) {
-    return get<IScriptWithVersions>({
+export function fetchScriptVersions({
+    name,
+    expandResponseWith,
+}: IScriptByNamePayload & IFetchScriptsOptions) {
+    return get<IScript[], IListResponse<IScript>>({
         url: API_URLS.SCRIPT_BY_NAME,
         pathParams: {
             name,
         },
+        queryParams: toExpandQueryParam(expandResponseWith),
+        // eslint-disable-next-line no-underscore-dangle
+        mapResponse: ({ data }) => data._embedded,
     });
 }
 
-export function fetchScript({ name, version }: IScriptByNameAndVersionPayload) {
+export function fetchScriptVersion({
+    name,
+    version,
+    expandResponseWith,
+}: IScriptByNameAndVersionPayload & IFetchScriptsOptions) {
     return get<IScript>({
         url: API_URLS.SCRIPT_BY_NAME_VERSION,
         pathParams: {
             name,
             version,
         },
+        queryParams: toExpandQueryParam(expandResponseWith),
     });
 }
 
-export function createScript(script: IScript) {
-    return post<IScript>({
+/**
+ * Makes a new 'script-version-combo', which can either be the first version of a totally new script,
+ * OR an extra version of an existing script.
+ */
+export function createScriptVersion(script: IScriptBase) {
+    return post<IScriptBase>({
         url: API_URLS.SCRIPTS,
         body: script,
     });
 }
 
-export function updateScript(script: IScript) {
-    return put<IScript>({
+/**
+ * Updates an existing 'script-version-combo'. This will NOT make a new version of the script,
+ * but just updates the fields of the existing one.
+ */
+export function updateScriptVersion(script: IScriptBase) {
+    return put<IScriptBase>({
         url: API_URLS.SCRIPT_BY_NAME_VERSION,
         pathParams: {
             name: script.name,
@@ -54,7 +81,16 @@ export function updateScript(script: IScript) {
     });
 }
 
-export function deleteScript({ name, version }: IScriptByNameAndVersionPayload) {
+export function deleteScriptVersions({ name }: IScriptByNamePayload) {
+    return remove<{}>({
+        url: API_URLS.SCRIPT_BY_NAME,
+        pathParams: {
+            name,
+        },
+    });
+}
+
+export function deleteScriptVersion({ name, version }: IScriptByNameAndVersionPayload) {
     return remove<{}>({
         url: API_URLS.SCRIPT_BY_NAME_VERSION,
         pathParams: {
@@ -62,4 +98,27 @@ export function deleteScript({ name, version }: IScriptByNameAndVersionPayload) 
             version,
         },
     });
+}
+
+function toExpandQueryParam(expandScriptsResponseWith: IExpandScriptsResponseWith = {}): IUrlParams {
+    const {
+        execution = true,
+        scheduling = true,
+    } = expandScriptsResponseWith;
+
+    const expandItems: string[] = [];
+    if (execution) {
+        expandItems.push('execution');
+    }
+    if (scheduling) {
+        expandItems.push('scheduling');
+    }
+
+    if (expandItems.length === 0) {
+        return {};
+    }
+
+    return {
+        expand: expandItems.join(','),
+    };
 }
