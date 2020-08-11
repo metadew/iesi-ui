67ff86719aaff783409b64577a373e5d6bc87a63
Dries Beerten <dries.beerten@kunstmaan.be>
2020-08-11 13:53:45 +0200

    clean up

--- src/state/initApp/index.ts
+++ src/state/initApp/index.ts
@@ -2,7 +2,7 @@ import listenForTranslationKeyToggles
     from '@snipsonian/react/es/components/i18n/translator/listenForTranslationKeyToggles';
 import { toggleTranslationKeys } from 'state/i18n/actions';
 import executePeriodicallyWhenBrowserTabActive from 'utils/tabVisibility/executePeriodicallyWhenBrowserTabActive';
-import { checkPollingExecutionRequests, addPollingExecutionRequest } from 'state/ui/actions';
+import { checkPollingExecutionRequests } from 'state/ui/actions';
 import { INTERVAL_IN_MILLIS } from 'config/state.config';
 import { getStore } from '../index';
 import { fetchEnvConfig } from '../envConfig/actions';
@@ -16,11 +16,7 @@ export default function initApp() {
         onToggle: () => dispatch(toggleTranslationKeys()),
     });
 
-    // TODO: testing! Remove this line when finished
-    dispatch(addPollingExecutionRequest({ id: 'bffb85ce-dafc-41c9-96d3-0de63b1c3393' }));
-
     executePeriodicallyWhenBrowserTabActive({
-        // toBeExecuted: () => dispatch(fetchGlobalConfig()),
         toBeExecuted: () => dispatch(checkPollingExecutionRequests()),
         intervalInMillis: INTERVAL_IN_MILLIS.CHECK_STATUS_EXECUTION_REQUESTS,
         executeImmediatelyInActiveTab: true,
