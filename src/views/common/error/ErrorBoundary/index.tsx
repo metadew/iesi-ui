b5b2f5f0187888c0457e7c66b03661aa4e2c9251
Dries Beerten <dries.beerten@kunstmaan.be>
2020-09-02 11:43:11 +0200

    add some styling to error page

--- src/views/common/error/ErrorBoundary/index.tsx
+++ src/views/common/error/ErrorBoundary/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import { ROUTE_KEYS } from 'views/routes';
-import RouteLink from 'views/common/navigation/RouteLink';
+import { Button, Paper, Typography, Box } from '@material-ui/core';
+import { ErrorOutlineRounded as ErrorIcon } from '@material-ui/icons';
+import ThemeProvider from 'views/appShell/ThemeProvider';
+import { redirectTo, ROUTE_KEYS } from 'views/routes';
+import Translate from '@snipsonian/react/es/components/i18n/Translate';
 
 interface IComponentProps {
     children: JSX.Element[] | JSX.Element;
@@ -19,6 +21,7 @@ export default class ErrorBoundary extends React.Component<IComponentProps, ICom
         };
 
         this.resetErrorBoundary = this.resetErrorBoundary.bind(this);
+        this.onRefreshClick = this.onRefreshClick.bind(this);
     }
 
     public componentDidCatch() {
@@ -29,16 +32,52 @@ export default class ErrorBoundary extends React.Component<IComponentProps, ICom
 
     public render() {
         if (this.state.hasError) {
-            // TODO render something nice in case of error
             return (
-                <RouteLink to={ROUTE_KEYS.R_HOME} exact>
-                    <Button onClick={this.resetErrorBoundary}>
-                        To home
-                    </Button>
-                </RouteLink>
+                <ThemeProvider
+                    render={() => (
+                        <Box height="100%">
+                            <Box
+                                position="relative"
+                                display="flex"
+                                flexDirection="column"
+                                minHeight="100%"
+                                justifyContent="center"
+                            >
+                                <Box width={540} maxWidth="90vw" marginX="auto">
+                                    <Paper>
+                                        <Box padding={3} textAlign="center">
+                                            <Box marginBottom={2} textAlign="center" fontSize={80} lineHeight={0}>
+                                                <ErrorIcon color="error" fontSize="inherit" />
+                                            </Box>
+                                            <Typography variant="h2" paragraph>
+                                                <Translate msg="error.default.title" />
+                                            </Typography>
+                                            <Typography variant="body1" paragraph>
+                                                <Translate msg="error.default.text" />
+                                            </Typography>
+                                            <Button
+                                                variant="contained"
+                                                color="primary"
+                                                size="small"
+                                                onClick={this.onRefreshClick}
+                                            >
+                                                <Translate msg="error.default.button" />
+                                            </Button>
+                                        </Box>
+                                    </Paper>
+                                </Box>
+                            </Box>
+                        </Box>
+                    )}
+                />
             );
         }
         return this.props.children;
+    }
+
+    private onRefreshClick() {
+        this.resetErrorBoundary();
+        redirectTo({ routeKey: ROUTE_KEYS.R_HOME });
     }
 
     private resetErrorBoundary() {
