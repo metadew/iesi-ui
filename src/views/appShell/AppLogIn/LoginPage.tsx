391e794a1037375df2d417b5becc01ea4e9244bc
Robbe Berrevoets <6872974+BerrevoetsRobbe@users.noreply.github.com>
2020-12-16 20:49:00 +0100

    use ui text fields and buttons

--- src/views/appShell/AppLogIn/LoginPage.tsx
+++ src/views/appShell/AppLogIn/LoginPage.tsx
@@ -1,10 +1,8 @@
-import React, { useContext } from 'react';
-import { TextField, Button, Container, Typography, Box } from '@material-ui/core';
+import React, { useContext, useState } from 'react';
+import { Button, Container, Typography, Box } from '@material-ui/core';
 import { useHistory, useLocation } from 'react-router-dom';
-import { Formik } from 'formik';
-import { object, string } from 'yup';
-// import * as Yup from 'yup';
-import { triggerFlashMessage } from 'state/ui/actions';
+import TextInput from 'views/common/input/TextInput';
+import Translate from '@snipsonian/react/es/components/i18n/Translate';
 import { ReactComponent as IesiLogo } from './logo.svg';
 import { UserSessionContext } from './contexts/UserSessionContext';
 import { logon } from '../../../api/security/security.api';
@@ -12,12 +10,41 @@ import { logon } from '../../../api/security/security.api';
 
 function Login() {
     const history = useHistory();
-
-    // Cast to any is required due to
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [hasSubmitErrors, setHasSubmitErrors] = useState(false);
     // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/41674
     const { from } = useLocation().state as any || { from: { pathname: '/' } };
     const userSession = useContext(UserSessionContext);
 
+
+    const handleSubmit = () => {
+        if (username !== '' && password !== '') {
+            setHasSubmitErrors(false);
+            // TODO: perform logic
+            logon({
+                username,
+                password,
+            })
+                .then(async (response) => {
+                    // eslint-disable-next-line max-len
+                    // check for error response
+                    if (response.accessToken) {
+                        console.log(from);
+                        userSession.setAuthenticated(response.accessToken);
+                        history.replace(from.pathname);
+                    }
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error);
+                })
+                .finally(() => {
+                    console.log('finally');
+                });
+        } else {
+            setHasSubmitErrors(true);
+        }
+    };
     return (
         <Container component="main" maxWidth="xs">
             <Box
@@ -32,118 +59,36 @@ function Login() {
                     IESI Automation
                 </Typography>
             </Box>
-            <Formik
-                initialValues={{ username: '', password: '' }}
-                validationSchema={object({
-                    username: string()
-                        .max(15, 'Must be 15 characters or less')
-                        .required('Required'),
-                    password: string()
-                        .required('Required'),
-                })}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        logon({
-                            username: values.username,
-                            password: values.password,
-                        })
-                            .then(async (response) => {
-                                // eslint-disable-next-line max-len
-                                triggerFlashMessage({ translationKey: 'username or/and password incorrect !' });
-                                // check for error response
-                                console.log(response);
-                                if (response.accessToken) {
-                                    console.log(from);
-                                    userSession.setAuthenticated(response.accessToken);
-                                    history.replace(from.pathname);
-                                }
-                            })
-                            .catch((error) => {
-                                // this.setState({ errorMessage: error });
-                                console.error('There was an error!', error);
-                                triggerFlashMessage({ translationKey: 'username or/and password incorrect !' });
-                            })
-                            .finally(() => {
-                                setSubmitting(false);
-                            });
-                        // fetch(`${iesiApiBaseUrl}//localhost:8080/api/users/login`, requestOptions)
-                        //     // eslint-disable-next-line consistent-return
-                        //     .then(async (response) => {
-                        //         const data = await response.json();
-                        //         // eslint-disable-next-line max-len
-                        //         triggerFlashMessage({ translationKey: 'username or/and password incorrect !' });
-                        //         console.log(process.env.PUBLIC_URL);
-                        //         // check for error response
-                        //         if (!response.ok) {
-                        //             // get error message from body or default to response status
-                        //             const error = (data && data.message) || response.status;
-                        //             return Promise.reject(error);
-                        //         }
-                        //         console.log(from);
-                        //         userSession.setAuthenticated(data.accessToken);
-                        //         history.replace('/scripts');
-                        //     })
-                        //     .catch((error) => {
-                        //         // this.setState({ errorMessage: error });
-                        //         console.error('There was an error!', error);
-                        //     })
-                        //     .finally(() => {
-                        //         setSubmitting(false);
-                        //     });
-                    }, 400);
-                }}
-            >
-                {({
-                    values,
-                    // eslint-disable-next-line no-shadow
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* and other goodies */
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <TextField
-                            id="username"
-                            label="Username"
-                            name="username"
-                            value={values.username}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            margin="normal"
-                            variant="outlined"
-                            required
-                            autoComplete="username"
-                            fullWidth
-                            color="primary"
-                        />
-                        <TextField
-                            id="password"
-                            label="Password"
-                            name="password"
-                            type="password"
-                            value={values.password}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            margin="normal"
-                            variant="outlined"
-                            required
-                            autoComplete="password"
-                            fullWidth
-                            color="primary"
-                        />
-                        <Button
-                            type="submit"
-                            disabled={isSubmitting}
-                            variant="contained"
-                            color="primary"
-                            fullWidth
-                        >
-                            Log in
-                        </Button>
-                    </form>
-                )}
-            </Formik>
+            <TextInput
+                id="username"
+                label="username"
+                margin="normal"
+                required
+                error={hasSubmitErrors && username === ''}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextInput
+                id="password"
+                label="password"
+                required
+                error={hasSubmitErrors && password === ''}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <Box textAlign="center" marginTop={5}>
+                <Button
+                    variant="contained"
+                    size="medium"
+                    color="primary"
+                    disableElevation
+                    onClick={handleSubmit}
+                >
+                    <Container component="main" maxWidth="xl">
+                        <Translate msg="login" />
+                    </Container>
+                </Button>
+            </Box>
         </Container>
     );
 }
